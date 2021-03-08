6a6124e6e354b440c3ce4997addfb257112f707e
donfrigo <donfrigo@gmail.com>
2021-03-08 17:06:58 +0100

    new commit

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t donfrigo/multi-client:latest -t donfrigo/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t donfrigo/multi-server:latest -t donfrigo/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t donfrigo/multi-worker -t donfrigo/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push donfrigo/multi-client:latest
+docker push donfrigo/multi-server:latest
+docker push donfrigo/multi-worker:latest
+docker push donfrigo/multi-client:$SHA
+docker push donfrigo/multi-server:$SHA
+docker push donfrigo/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=donfrigo/multi-server:$SHA
+kubectl set image deployments/client-deployment server=donfrigo/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=donfrigo/multi-worker:$SHA
